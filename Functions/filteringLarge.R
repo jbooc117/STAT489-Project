dc6649868c8c78c294744666d364c6740b039701
jbooc117 <158986907+jbooc117@users.noreply.github.com>
2024-03-15 18:16:08 -0500

    updates method of obtaining dataset

--- Functions/filteringLarge.R
+++ Functions/filteringLarge.R
@@ -2,30 +2,13 @@ library(tidyverse)
 library(sf)
 library(ggfortify)
 
-#Initial testing for what variables we could filter out
-df.largeCVD = filter(Rates_and_Trends_in_Heart_Disease_and_Stroke_Mortality_Among_US_Adults_35_by_County_Age_Group_Race_Ethnicity_and_Sex_2000_2019_20240208
-                     , LocationAbbr == "TX" & Data_Value != "NA")
-View(df.largeCVD)
-df.largeCVD = subset(df.largeCVD, select = -c(StratificationCategory1,StratificationCategory2,StratificationCategory3))
-df.largeCVD = subset(df.largeCVD, select = -c(Topic, Data_Value_Footnote_Symbol, Data_Value_Footnote))
-
-count(df.largeCVD, LocationDesc == "Anderson" & Year == 2009)
-df.largeCVD = filter(df.largeCVD, Stratification3 != "Overall")
-
-View(df2.largeCVD)
-count(df.largeCVD, Stratification2 != "Overall")
-
-df.largeCVD = subset(df.largeCVD, select = -c(GeographicLevel, DataSource))
-
-df.wideCVD = 
-  
 #Shombit code, used to start the creation of finalCVD
   
 data.Tx <- subset(Rates_and_Trends_in_Heart_Disease_and_Stroke_Mortality_Among_US_Adults_35_by_County_Age_Group_Race_Ethnicity_and_Sex_2000_2019_20240208)
 data.CD <- subset(data.Tx, Topic == "Cardiovascular disease (CVD)")
 data.CD2 = subset(data.CD, select = -c(GeographicLevel, DataSource,Topic, Data_Value_Type,Data_Value_Footnote_Symbol,Data_Value_Footnote, Confidence_limit_High,Confidence_limit_Low, StratificationCategory1, StratificationCategory2, StratificationCategory3))
-Data.CD3 = filter(data.CD2, Stratification2 != "Overall")
-Data.CD4 <- filter(Data.CD3, Data_Value != "NA")
+Data.CD3 = data.CD2
+Data.CD4 <- filter(data.CD2, Data_Value != "NA")
 Data.CD4 = subset(Data.CD4, select = -c(Stratification3))
 View(Data.CD4)
 #wideCVD = pivot_wider(Data.CD4)
@@ -34,22 +17,26 @@ View(Data.CD4)
 #Our data does not have longitude/latitude, but we were able to add it by matching the GEOID to the data from
 #the US_Conus_VacSocial 
 load("VacData.Rdata")
+Data.CD5 = bind_cols(Data.CD4, US_Conus_VacSocial)
 mergedData = merge(Data.CD4, US_Conus_VacSocial, by.x = "LocationID", by.y = "GEOID", all.x = TRUE) 
+View(mergedData)
 
-finalCVD = slice(mergedData, 1:248883)
-View(finalCVD)
+marchCVD = subset(mergedData, select = c(LocationID, County, State,
+                                         lon, lat, perc_white,
+                                         perc_africanamerican,
+                                         perc_hispanic,
+                                         perc_asian,
+                                         Year, Data_Value,
+                                         Data_Value_Unit,
+                                         Stratification2,
+                                         geometry))
+View(marchCVD)
+marchCVD = marchCVD%>%filter(Data_Value_Unit != "%") #removes year ranges
+na.omit(marchCVD)
 
-#Removing the extra variables added by the merging (probably could have just selected the original columns)
-finalCVD = subset(finalCVD, select = -c(LocationID, County, State, perc_vac, population,
-                                          population.persqkm, median_householdincome,
-                                          perc_HealthInsurance))
-View(finalCVD)
-write_csv(finalCVD, file = "C:/Users/jbooc/Documents/R Coding/STAT489-Project/finalCVD3.csv")
 
-library(tidyverse)
-library(sf)
-library(ggfortify)
 
+### Previous Attempts
 #Initial testing for what variables we could filter out
 df.largeCVD = filter(Rates_and_Trends_in_Heart_Disease_and_Stroke_Mortality_Among_US_Adults_35_by_County_Age_Group_Race_Ethnicity_and_Sex_2000_2019_20240208
                      , LocationAbbr == "TX" & Data_Value != "NA")
@@ -66,79 +53,3 @@ count(df.largeCVD, Stratification2 != "Overall")
 df.largeCVD = subset(df.largeCVD, select = -c(GeographicLevel, DataSource))
 
 df.wideCVD = 
-  
-#Shombit code, used to start the creation of finalCVD
-  
-data.Tx <- subset(Rates_and_Trends_in_Heart_Disease_and_Stroke_Mortality_Among_US_Adults_35_by_County_Age_Group_Race_Ethnicity_and_Sex_2000_2019_20240208)
-data.CD <- subset(data.Tx, Topic == "Cardiovascular disease (CVD)")
-data.CD2 = subset(data.CD, select = -c(GeographicLevel, DataSource,Topic, Data_Value_Type,Data_Value_Footnote_Symbol,Data_Value_Footnote, Confidence_limit_High,Confidence_limit_Low, StratificationCategory1, StratificationCategory2, StratificationCategory3))
-Data.CD3 = filter(data.CD2, Stratification2 != "Overall")
-Data.CD4 <- filter(Data.CD3, Data_Value != "NA")
-Data.CD4 = subset(Data.CD4, select = -c(Stratification3))
-View(Data.CD4)
-wideCVD = pivot_wider(Data.CD4)
-
-
-#Our data does not have longitude/latitude, but we were able to add it by matching the GEOID to the data from
-#the US_Conus_VacSocial 
-load("VacData.Rdata")
-mergedData = merge(Data.CD4, US_Conus_VacSocial, by.x = "LocationID", by.y = "GEOID", all.x = TRUE) 
-
-finalCVD = slice(mergedData, 1:248883)
-View(finalCVD)
-
-#Removing the extra variables added by the merging (probably could have just selected the original columns)
-finalCVD = subset(finalCVD, select = -c(LocationID, County, State, perc_vac, population,
-                                          population.persqkm, perc_white, perc_africanamerican,
-                                          perc_hispanic, perc_asian, median_householdincome,
-                                          perc_HealthInsurance))
-View(finalCVD)
-write_csv(finalCVD, file = "C:/Users/jbooc/Documents/R Coding/STAT489-Project/finalCVD2.csv")
-library(tidyverse)
-library(sf)
-library(ggfortify)
-
-#Initial testing for what variables we could filter out
-df.largeCVD = filter(Rates_and_Trends_in_Heart_Disease_and_Stroke_Mortality_Among_US_Adults_35_by_County_Age_Group_Race_Ethnicity_and_Sex_2000_2019_20240208
-                     , LocationAbbr == "TX" & Data_Value != "NA")
-View(df.largeCVD)
-df.largeCVD = subset(df.largeCVD, select = -c(StratificationCategory1,StratificationCategory2,StratificationCategory3))
-df.largeCVD = subset(df.largeCVD, select = -c(Topic, Data_Value_Footnote_Symbol, Data_Value_Footnote))
-
-count(df.largeCVD, LocationDesc == "Anderson" & Year == 2009)
-df.largeCVD = filter(df.largeCVD, Stratification3 != "Overall")
-
-View(df2.largeCVD)
-count(df.largeCVD, Stratification2 != "Overall")
-
-df.largeCVD = subset(df.largeCVD, select = -c(GeographicLevel, DataSource))
-
-df.wideCVD = 
-  
-#Shombit code, used to start the creation of finalCVD
-  
-data.Tx <- subset(Rates_and_Trends_in_Heart_Disease_and_Stroke_Mortality_Among_US_Adults_35_by_County_Age_Group_Race_Ethnicity_and_Sex_2000_2019_20240208)
-data.CD <- subset(data.Tx, Topic == "Cardiovascular disease (CVD)")
-data.CD2 = subset(data.CD, select = -c(GeographicLevel, DataSource,Topic, Data_Value_Type,Data_Value_Footnote_Symbol,Data_Value_Footnote, Confidence_limit_High,Confidence_limit_Low, StratificationCategory1, StratificationCategory2, StratificationCategory3))
-Data.CD3 = filter(data.CD2, Stratification2 != "Overall")
-Data.CD4 <- filter(Data.CD3, Data_Value != "NA")
-Data.CD4 = subset(Data.CD4, select = -c(Stratification3))
-View(Data.CD4)
-wideCVD = pivot_wider(Data.CD4)
-
-
-#Our data does not have longitude/latitude, but we were able to add it by matching the GEOID to the data from
-#the US_Conus_VacSocial 
-load("VacData.Rdata")
-mergedData = merge(Data.CD4, US_Conus_VacSocial, by.x = "LocationID", by.y = "GEOID", all.x = TRUE) 
-
-finalCVD = slice(mergedData, 1:248883)
-View(finalCVD)
-
-#Removing the extra variables added by the merging (probably could have just selected the original columns)
-finalCVD = subset(finalCVD, select = -c(LocationID, County, State, perc_vac, population,
-                                          population.persqkm, perc_white, perc_africanamerican,
-                                          perc_hispanic, perc_asian, median_householdincome,
-                                          perc_HealthInsurance))
-View(finalCVD)
-write_csv(finalCVD, file = "C:/Users/jbooc/Documents/R Coding/STAT489-Project/finalCVD2.csv")
